2a641d32a6c7a0f7ede764621ce7f03161be6a5b
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-01-17 18:43:38 -0500

    functioning script with minor edits to html and css

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,11 @@ var startButtonEl = document.getElementById("startButton");
 var welcomeMessageEl = document.getElementById("welcomeMessage");
 var qContainerEl = document.getElementById("questionsContainer");
 var qElement = document.getElementById("question");
-var answerButtonsEl = document.getElementById("answers");
-var countdownEl = document.getElementById("timerArea");
+var answerButtonEl = document.getElementById("answers");
+var timerAreaEl = document.getElementById("timerArea");
 var scoreAreaEl = document.getElementById("scoreArea");
 var highScoresButtonEl = document.getElementById("showScoresButton");
+var answerResultEl = document.getElementById("answerResult");
 
 var timer = 75;
 var runningTimer;
@@ -15,15 +16,150 @@ var qNumber;
 var finalScore;
 const MAX_HIGH_SCORES = 7;
 
-startButtonEl.addEventListener("click", startGame);
 
-function startGame() {
-    startButton.classList.add("hide");
-    welcomeMessage.classList.add("hide");
-    scoreArea.classList.add("hide");
-    answerButtons.classList.remove("hide");
+//High Scores grab
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+
+startButtonEl.addEventListener("click", startQuiz);
+highScoresButtonEl.addEventListener("click", displayScores);
+
+//main function
+function startQuiz() {
+    startButtonEl.classList.add("hide");
+    welcomeMessageEl.classList.add("hide");
+    scoreAreaEl.classList.add("hide");
+    answerButtonEl.classList.remove("hide");
+    qNumber = 0;
+    qContainerEl.classList.remove("hide");
+    scoreAreaEl.innerHTML = "";
+    startTimer();
+    while (answerButtonEl.firstChild) {
+        answerButtonEl.removeChild(answerButtonEl.firstChild);
+    }
+    showQuestion(questions[qNumber]);
+    highScoresButtonEl.classList.remove("hide");
 }
 
+//questions generator
+function showQuestion(question) {
+    qElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+        button.dataset.correct = answer.correct;
+        } 
+        button.addEventListener("click", selectAnswer);
+        answerButtonEl.appendChild(button);
+    });
+}
+
+//timer
+function startTimer() {
+    timerAreaEl.innerHTML = "Time Remaining: " + timer;
+    if (timer <= 0) {
+        endQuiz();
+    } else {
+        timer -= 1;
+        runningTimer = setTimeout(startTimer, 1000);
+    }
+}
+
+//check answers and penalize time, show when wrong answer
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    if (!selectedButton.dataset.correct) {
+        timer = timer - 10;
+        timerAreaEl.innerText = "Incorrect!";
+    }
+    if (qNumber == questions.length - 1) {
+        endQuiz();
+    } else {
+        clearQuestion();
+        qNumber++;
+        showQuestion(questions[qNumber]);
+    }
+}
+
+
+
+
+
+function clearQuestion() {
+    while (answerButtonEl.firstChild) {
+        answerButtonEl.removeChild(answerButtonEl.firstChild);
+    }
+}
+
+
+function endQuiz() {
+    clearInterval(runningTimer);
+    timerAreaEl.innerText = "Finished";
+    clearQuestion();
+    showResults();
+    startButtonEl.innerText = "Restart";
+    startButtonEl.classList.remove("hide");
+    timer = 75;
+    score = 0;
+}
+
+function showResults() {
+    finalScore = timer;
+    if (finalScore < 0) {
+        finalScore = 0;
+    }
+    qElement.innerText = "";
+    scoreAreaEl.classList.remove("hide");
+    answerButtonEl.classList.add("hide");
+    scoreAreaEl.innerHTML = `Your score is ${finalScore}!<div id="init">Name: <input type="text" name="initials" id="initials" placeholder="Enter Your Name"><button id="save-btn" class="save-btn btn" onclick="submitScores(event)" disabled>Save</button>`;
+    username = document.getElementById("initials");
+    saveButton = document.getElementById("save-btn");
+    username.addEventListener("keyup", function() {
+        saveButton.disabled = !username.value;
+    });
+}
+
+
+function submitScores(e) {
+    const score = {
+        score: finalScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    displayScores();
+}
+
+
+function displayScores() {
+    clearInterval(runningTimer);
+    timerAreaEl.innerHTML = "";
+    clearQuestion();
+    qElement.innerText = "";
+    scoreAreaEl.classList.remove("hide");
+
+    scoreAreaEl.innerHTML = `<h2>High Scores</h2><ul id="highScoresList"></ul><button id="clearScores" class="btn" onclick="clearScores()">Clear Scores</button>`;
+    const highScoresList = document.getElementById("highScoresList");
+    highScoresList.innerHTML = highScores
+        .map(score => {
+        return `<li class="scoresList">${score.name} - ${score.score}</li>`;
+        })
+        .join("");
+    startButtonEl.classList.remove("hide");
+    highScoresButtonEl.classList.add("hide");
+}
+
+
+function clearScores() {
+    highScores = [];
+    highScoresList.innerHTML = "<h3>Scores have been Cleared</h3>";
+    document.getElementById("clearScores").classList.add("hide");
+}
 
 var questions = [
     {
